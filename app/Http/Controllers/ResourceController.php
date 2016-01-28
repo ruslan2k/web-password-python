a2ea460fced7b78b04bfd0d0abeb4569028caee0
Ruslan Karachun <free.changer@gmail.com>
2016-01-28 23:24:27 +0300

    Add repository

--- app/Http/Controllers/ResourceController.php
+++ app/Http/Controllers/ResourceController.php
@@ -2,21 +2,44 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Resource;
 use App\Http\Requests;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Repositories\ResourceRepository;
+
 
 class ResourceController extends Controller
 {
+    /**
+     * The resources repository instance.
+     *
+     * @var ResourceRepository
+     *
+     */
+    protected $resources;
 
-    public function index ()
+    /**
+     * Create a new controller instance.
+     *
+     * @param ResourceRepository $resources
+     * @return void
+     */
+    public function __construct (ResourceRepository $resources)
     {
-        $resources = Resource::all();
+        $this->middleware('auth');
 
-        //return dump($resources);
-        return view('resource.index', ['resources' => $resources]);
+        $this->resources = $resources;
+    }
+
+    public function index (Request $request)
+    {
+        //$resources = Resource::all();
+        $resources = Resource::where('user_id', $request->user()->id)->get();
+
+        return view('resource.index', [
+            'resources' => $resources,
+        ]);
     }
 
     public function show (Resource $resource)
--- database/migrations/2016_01_13_082356_create_resources_table.php
+++ database/migrations/2016_01_13_082356_create_resources_table.php
@@ -14,6 +14,7 @@ class CreateResourcesTable extends Migration
     {
         Schema::create('resources', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->index();
             $table->string('name');
             $table->timestamps();
         });
