812dc04a636a7818a01fbb75d2348b7c0752d974
Ruslan Karachun <free.changer@gmail.com>
2017-01-08 19:19:10 +0000

    Added icon

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,9 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
+
 
 from django.contrib import admin
 
@@ -15,6 +18,7 @@ urlpatterns = [
     url(r"^account/signup/$", SignupView.as_view(), name="account_signup"),
     url(r"^account/login/$", LoginView.as_view(), name="account_login"),
     url(r"^account/", include("account.urls")),
+    url(r"^favicon.ico$", RedirectView.as_view(url=staticfiles_storage.url("favicon.ico"), permanent=False), name="favicon"),
     url(r"^resources/", include("resources.urls")),
 ]
 
