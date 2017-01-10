569bdbb754dd1a84d172d77e5d209144b04eef98
Ruslan Karachun <free.changer@gmail.com>
2017-01-10 10:36:39 +0000

    Save for production

--- mysite/settings.py
+++ mysite/settings.py
@@ -7,7 +7,7 @@ PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir
 PACKAGE_ROOT = os.path.abspath(os.path.dirname(__file__))
 BASE_DIR = PACKAGE_ROOT
 
-DEBUG = True
+DEBUG = False
 
 DATABASES = {}
 
