e3ee1f4b3a27012859a8a8413a9508b46ee065a7
Ruslan Karachun <free.changer@gmail.com>
2019-04-15 13:55:39 +0000

    Added some tests

--- js/src/password.js
+++ js/src/password.js
@@ -1,22 +1,40 @@
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
+const crypto = require('crypto')
+const uuidv4 = require('uuid/v4');
 
 const adapter = new FileSync('db.json')
 const db = low(adapter)
 
 // Set some defaults (required if your JSON file is empty)
-db.defaults({ posts: [], user: {}, count: 0 })
+db.defaults({ profiles: [], user: {}, count: 0 })
   .write()
-
 // Add a post
-db.get('posts')
-  .push({ id: 1, title: 'lowdb is awesome'})
-  .write()
-
+// db.get('posts')
+//   .push({ id: 1, title: 'lowdb is awesome'})
+//   .write()
 // Set a user using Lodash shorthand syntax
-db.set('user.name', 'typicode')
-  .write()
-
+// var retVal = db.set('user.name', 'typicode')
+//   .write()
 // Increment count
-db.update('count', n => n + 1)
-  .write()
+// db.update('count', n => n + 1)
+//   .write()
+
+function createUserProfile (userId) {
+  db.get('profiles')
+    .push({ id: uuidv4(), userId: userId, salt: crypto.randomBytes(25).toString('HEX') })
+    .write()
+}
+// Set a user using Lodash shorthand syntax
+
+function addGroup (password) {
+
+}
+
+// console.log(CryptoJS.PBKDF2("password", "abc").toString())
+// console.log(CryptoJS.HmacSHA1("Message", "Key").toString());
+// console.log(typeof CryptoJS.HmacSHA1("Message", "Key"));
+createUserProfile(1)
+module.exports = {
+  addGroup
+}
