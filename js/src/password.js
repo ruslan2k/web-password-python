9ac76b33380c71c37203844241d3d47cb4cc0f63
Ruslan Karachun <free.changer@gmail.com>
2019-05-03 14:26:33 +0000

    Added Priv-Pub key pair

--- js/src/password.js
+++ js/src/password.js
@@ -7,24 +7,39 @@ const adapter = new FileSync('db.json')
 const db = low(adapter)
 
 // Set some defaults (required if your JSON file is empty)
-db.defaults({ profiles: [], user: {}, count: 0 })
+db.defaults({ profiles: [], user: {}})
   .write()
-// Add a post
-// db.get('posts')
-//   .push({ id: 1, title: 'lowdb is awesome'})
-//   .write()
-// Set a user using Lodash shorthand syntax
-// var retVal = db.set('user.name', 'typicode')
-//   .write()
-// Increment count
-// db.update('count', n => n + 1)
-//   .write()
 
-function createUserProfile (userId) {
+function createUserProfile (userId, password) {
+  const salt = crypto.randomBytes(32).toString('HEX')
+  const key = crypto.pbkdf2Sync(password, salt, 1000, 32, 'sha512')
+  const profileId = uuidv4()
+  const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
+    modulusLength: 4096,
+    publicKeyEncoding: {
+      type: 'spki',
+      format: 'pem'
+    },
+    privateKeyEncoding: {
+      type: 'pkcs8',
+      format: 'pem',
+      cipher: 'aes-256-cbc',
+      passphrase: key.toString('hex')
+    }
+  });
   db.get('profiles')
-    .push({ id: uuidv4(), userId: userId, salt: crypto.randomBytes(25).toString('HEX') })
+    .push({
+      id: profileId,
+      userId: userId,
+      salt: salt,
+      publicKey: publicKey,
+      encryptedPrivateKey: privateKey,
+    })
     .write()
+  return profileId
 }
+
+// function createPasswordForPrivateKey (password
 // Set a user using Lodash shorthand syntax
 
 function addGroup (password) {
@@ -34,7 +49,10 @@ function addGroup (password) {
 // console.log(CryptoJS.PBKDF2("password", "abc").toString())
 // console.log(CryptoJS.HmacSHA1("Message", "Key").toString());
 // console.log(typeof CryptoJS.HmacSHA1("Message", "Key"));
-createUserProfile(1)
+const profileId = createUserProfile(1, 'p@$$W0rD')
+const profile = db.get('profiles').find({ id: profileId }).value()
+console.log(profile)
+
 module.exports = {
   addGroup
 }
