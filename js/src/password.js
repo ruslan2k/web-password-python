44ccb54555f91c74a5dfefed7aea76a40ec4af0d
Ruslan Karachun <free.changer@gmail.com>
2019-05-26 15:16:42 +0300

    Added secret files

--- js/src/password.js
+++ js/src/password.js
@@ -1,14 +1,38 @@
 const low = require('lowdb')
+const fs = require('fs')
 const FileSync = require('lowdb/adapters/FileSync')
 const crypto = require('crypto')
-const uuidv4 = require('uuid/v4');
+const path = require('path')
+const uuidv4 = require('uuid/v4')
 
 const adapter = new FileSync('db.json')
 const db = low(adapter)
+const passwordFile = process.env.passwordFile || path.join(__dirname, '.env.passwordFile')
+const publicKeyFile = process.env.publicKeyFile || path.join(__dirname, '.env.publicKeyFile')
+const encryptedPrivKeyFile = process.env.encryptedPrivKeyFile || path.join(__dirname, '.env.encryptedPrivKeyFile')
+var cSPassword
+
+if (fs.existsSync(passwordFile) && fs.readFileSync(passwordFile)) {
+  cSPassword = fs.readFileSync(passwordFile)
+} else {
+  cSPassword = crypto.randomBytes(32).toString('hex')
+  fs.writeFileSync(passwordFile, cSPassword)
+}
+
 
 // Set some defaults (required if your JSON file is empty)
-db.defaults({ profiles: [], user: {}})
+db.defaults({ groups: [], profiles: [], user: {}})
   .write()
+
+function createGroup (userId, name) {
+  var profile = db.get('profiles')
+    .find({ userId: userId })
+    .value()
+  if (!profile) {
+    return null
+  }
+  console.log('profile', profile)
+}
 
 function createUserProfile (userId, password) {
   const salt = crypto.randomBytes(32).toString('HEX')
@@ -46,13 +70,13 @@ function addGroup (password) {
 
 }
 
-// console.log(CryptoJS.PBKDF2("password", "abc").toString())
-// console.log(CryptoJS.HmacSHA1("Message", "Key").toString());
-// console.log(typeof CryptoJS.HmacSHA1("Message", "Key"));
-const profileId = createUserProfile(1, 'p@$$W0rD')
-const profile = db.get('profiles').find({ id: profileId }).value()
-console.log(profile)
+function test () {
+  const profileId = createUserProfile(1, 'p@$$W0rD')
+  const profile = db.get('profiles').find({ id: profileId }).value()
+  console.log(profile)
+}
 
 module.exports = {
-  addGroup
+  addGroup,
+  createGroup
 }
