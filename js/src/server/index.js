58e416551f14e1125993f304ec308144f4b6b0fc
Ruslan Karachun <free.changer@gmail.com>
2019-06-12 12:52:23 +0300

    infra: added static serv, added jobs

--- js/src/server/index.js
+++ js/src/server/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const path = require('path')
 dotenv.config()
 const app = express()
 
-app.get('/', (req, res) => res.json({ ok: true }))
+app.use(express.static(path.join(__dirname, '../client/build')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port: ' + server.address().port)
